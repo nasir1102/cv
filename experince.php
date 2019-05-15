b062e14df4bba8b67b46aecf678bf0d90ff1b120
nasir1102 <nasirhan13@gmail.com>
2019-05-15 19:30:03 +0700

    test

---
+++ experince.php
@@ -0,0 +1,44 @@
+
+<section class="resume-section p-3 p-lg-5 d-flex justify-content-center" id="experience">
+      <div class="w-100">
+        <h2 class="mb-5"></h2>
+
+        <div class="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
+          <div class="resume-content">
+            <h3 class="mb-0">Senior Web Developer</h3>
+            <img src="img/r3.jpg " width = "800 px"; >
+            <div class="subheading mb-3">Intelitec Solutions</div>
+            <p>Bring to the table win-win survival strategies to ensure proactive domination. At the end of the day,
+              going forward, a new normal that has evolved from generation X is on the runway heading towards a
+              streamlined cloud solution. User generated content in real-time will have multiple touchpoints for
+              offshoring.</p>
+          </div>
+          <div class="resume-date text-md-right">
+            <span class="text-primary">March 2013 - Present</span>
+            <?php
+            include("popup.php");
+            ?>
+
+          </div>
+        </div>
+
+        <div class="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
+          <div class="resume-content">
+            <h3 class="mb-0">Web Developer</h3>
+            <img src="img/r2.jpg" width = "800 px";>
+            <div class="subheading mb-3">Intelitec Solutions</div>
+            <p>Capitalize on low hanging fruit to identify a ballpark value added activity to beta test. Override the
+              digital divide with additional clickthroughs from DevOps. Nanotechnology immersion along the information
+              highway will close the loop on focusing solely on the bottom line.</p>
+          </div>
+          <div class="resume-date text-md-right">
+            <span class="text-primary">December 2011 - March 2013</span>
+            <?php
+            include("popup2.php");
+            ?>
+          </div>
+        </div>
+
+      </div>
+
+    </section>
---
+++ menu.php
@@ -0,0 +1,8 @@
+<div id="button"><a href="#popup">Pesan</a></div>
+    
+    <div id="popup">
+    	<div class="window">
+        	<a href="#" class="close-button" title="Close">X</a>
+            <h2>Selamat Datang di DUMET School</h2>
+        </div>
+    </div>
